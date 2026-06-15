19b86584572822b5523bf22e1232d1a16cadb27d
Abhishek Nayak <abhishek.gouri@gmail.com>
2026-06-16 02:47:21 +0530

    KV operations

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,3 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
 
-export default defineCloudflareConfig({
-	incrementalCache: kvIncrementalCache
-});
+export default defineCloudflareConfig({});
